f3891f9ef4ecf014ef3fe945167cc1d74ad54447
saeedp <saeed.pournejad@proton.me>
2023-09-26 15:30:36 +0330

    init

---
+++ 01.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿Solution();
+
+void Solution()
+{
+    var entry = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(entry))
+        // throw new Exception("no valid input");
+    {
+        Console.WriteLine(0);
+        return;
+    }
+    var numbers = entry.Trim().Split();
+
+    if (numbers.Length != 2)
+    {
+        Console.WriteLine(0);
+        // throw new Exception("should Input 2 numbers");
+        return;
+    }
+    var validRange = new Range(1, 100);
+
+    if (!IsValid(numbers[0], validRange))
+    {
+        // throw new Exception($"{numbers[0]} is not number or it is not between [1,100]");
+        Console.WriteLine(0);
+        return;
+    }
+
+    if (!IsValid(numbers[1], validRange))
+    {
+        // throw new Exception($"{numbers[1]} is not number or it is not between [1,100]");
+        Console.WriteLine(0);
+        return;
+    }
+
+
+    Console.WriteLine(Convert.ToByte(numbers[0]) + Convert.ToByte(numbers[1]));
+}
+
+bool IsValid(string input, Range validRange)
+{
+    return byte.TryParse(input, out var val) && val >= validRange.Start.Value && val <= validRange.End.Value;
+}
---
+++ 02.DegreeToRadian/Program.cs
@@ -0,0 +1,12 @@
+﻿var entry = Console.ReadLine();
+
+const double radian = Math.PI / 180.00;
+
+if (double.TryParse(entry, out var value) && value is >= 0 and <= 360)
+{
+    Console.WriteLine(value * radian);
+}
+else
+{
+    Console.WriteLine(-1);
+}
