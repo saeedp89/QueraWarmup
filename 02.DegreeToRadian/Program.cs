204d530b2a705c3390e59c4c56aca88c872b14a5
saeedp <saeed.pournejad@proton.me>
2023-09-26 15:43:40 +0330

    removed extra lines

--- 02.DegreeToRadian/Program.cs
+++ 02.DegreeToRadian/Program.cs
@@ -1,12 +1,6 @@
 ﻿var entry = Console.ReadLine();
 
-const double radian = Math.PI / 180.00;
-
 if (double.TryParse(entry, out var value) && value is >= 0 and <= 360)
 {
-    Console.WriteLine(value * radian);
-}
-else
-{
-    Console.WriteLine(-1);
+    Console.WriteLine(value * (Math.PI / 180.00));
 }
